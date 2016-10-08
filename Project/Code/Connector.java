74eb1661b2552bbb91db79d4ba620936bd2c4bea
Anudeep Rentala <anudeep.rentala@gmail.com>
2016-10-08 16:41:00 -0700

    Create Connector.java

---
+++ Project/Code/Connector.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Connector here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Connector extends Actor
+{
+    public int xcoor;
+    public int ycoor;
+    
+    public House in;
+    public House out;
+    public Path type;
+    /**
+     * Act - do whatever the Connector wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    public void select(){
+        
+    }
+}
