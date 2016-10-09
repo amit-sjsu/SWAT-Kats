87b3064e3db731297b7343793035cf84f67d1f86
Swathi Koduri <swathikoduri06@gmail.com>
2016-10-09 15:04:29 -0700

    Add files via upload
    House class

---
+++ Project/Code/House.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class House here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class House extends Actor
+{
+    
+    public int xcoor;
+    public int ycoor;
+
+    public House()
+    {
+        GreenfootImage image = getImage();
+        image.scale(150,180);
+    }
+
+    public void act()
+    {
+    	
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void connectByPath()
+    {
+        // put your code here
+        
+    }
+}
