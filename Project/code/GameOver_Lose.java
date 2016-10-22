593ace462988f81869d2504c5c9c1687d79b617a
shafidayatar <shafi.dayatar@gmail.com>
2016-10-21 21:25:39 -0700

    Reverting Previous Changes along with mine

---
+++ Project/code/GameOver_Lose.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameOver_Lose here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOver_Lose extends World
+{
+    private GreenfootSound backgroundMusic = new GreenfootSound("atebrains.wav");
+    /**
+     * Constructor for objects of class GameOver_Lose.
+     * 
+     */
+    public GameOver_Lose()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+        prepare();
+        backgroundMusic.play();
+    }
+    private void prepare()
+    {  
+        Replay objReplay = new Replay();
+        addObject(objReplay, 700, 505);   
+    }
+    public void stopBackgroundMusic()
+    {
+        backgroundMusic.stop();
+    }
+}
