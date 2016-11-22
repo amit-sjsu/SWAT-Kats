e3f952c96840c23048c2fe6c30ca78b05bc2cedd
navijain90 <navneet.jain@sjsu.edu>
2016-11-21 22:59:57 -0800

    Changes in timer class

--- Project/Game_Code/Timer.java
+++ Project/Game_Code/Timer.java
@@ -4,19 +4,28 @@ import java.util.ArrayList;
  
 public class Timer extends Actor
 {
-    private int timer=60;
+    private static String time;
+    
     public Timer()
     {
-        this("");
+        //this("");
+        this.time="";
     }
  
     public Timer(String Time)
     {
+        
         setTime(Time);
     }
  
     public void setTime(String Time)
     {
+        this.time=Time;
         setImage(new GreenfootImage(Time, 24, Color.GREEN, new Color(0, 0, 0, 0)));
-    } 
+    }
+    public String getTime(){
+        String[] parts = this.time.split(":");
+        System.out.println(parts[1].trim());
+        return parts[1].trim();
+    }
 }
