ac0f55e5424b120971bdf4d292f7de8db7a4d0e2
navijain90 <navneet.jain@sjsu.edu>
2016-11-29 14:57:26 -0800

    Cloud related stuff

--- Project/TheMuddyGame_Restlet/src/server/MuddyGameServer.java
+++ Project/TheMuddyGame_Restlet/src/server/MuddyGameServer.java
@@ -18,7 +18,7 @@ public class MuddyGameServer{
         Component component=new Component();
 
         // Add a new HTTP server listening on port 8080.  
-        component.getServers().add(Protocol.HTTP, 8091);  
+        component.getServers().add(Protocol.HTTP, 8089);  
 
         // Attach the sample application.  
         component.getDefaultHost().attach("/restlet", new InboundRoute());  
